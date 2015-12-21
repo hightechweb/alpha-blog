560481fc15cd7816aef91243e701005f9a2430a6
hightechweb <dtrimper888@gmail.com>
2015-12-21 20:53:58 +0000

    Add article and category M to M association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151220003358) do
+ActiveRecord::Schema.define(version: 20151221203411) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
